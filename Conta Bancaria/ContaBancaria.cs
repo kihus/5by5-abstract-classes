7dad46add31190d89fa5d6381f93e8ef82ce5c1a
Bruno Nascimento <Bruno Nascimento@DESKTOP-3M9QPCB>
2025-10-14 20:26:56 -0300

    feat: 'ContaBancaria' working correctly

---
+++ Conta Bancaria/ContaBancaria.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Conta_Bancaria;
+internal class ContaBancaria
+{
+    private string? _titular;
+    private decimal _saldo;
+
+    public void SetTitular(string titular)
+    {
+        bool ehNumero = titular.All(char.IsDigit);
+        while (titular == "")
+        {
+            Console.WriteLine("Seu nome não pode estar vazio.");
+            Console.Write("Qual o nome do titular da conta? ");
+            titular = Console.ReadLine() ?? "";
+        }
+        _titular = titular;
+    }
+
+    public void Depositar(decimal valor)
+    {
+        while(valor <= 1.0m)
+        {
+            Console.WriteLine("Valor informado é menor que o exigido!");
+            Console.WriteLine("\nPara utilizar as funções da conta, precisa fazer um desposito acima de R1,00");
+            Console.Write("Digite o valor que vai ser depositado: ");
+            valor = decimal.Parse(Console.ReadLine());
+        }
+        _saldo += valor;
+        Extrato();
+    }
+
+    public void Sacar(decimal valor)
+    {
+        while (valor > _saldo)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nValor maior que o saldo.");
+            Console.ResetColor();
+
+            Console.Write("Digite o valor que vai ser Sacado: ");
+            valor = decimal.Parse(Console.ReadLine());
+
+        }
+        
+            _saldo -= valor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n\tSaque efetuado.");
+            Console.ResetColor();
+            Extrato();
+        
+    }
+
+    public void Extrato()
+    {
+
+        Console.WriteLine($"\n| Banco Nubank\t   Agencia: 8888 |");
+        Console.WriteLine("| \t\t\t\t |");
+        Console.WriteLine("| Titular: " + _titular + "\t |");
+        Console.WriteLine("| \t\t\t\t |");
+        Console.WriteLine("| Saldo da conta: " + _saldo.ToString("F2") + "\t |");
+        Console.WriteLine("| \t\t\t\t |");
+        Console.WriteLine(@"|/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\|");
+     
+    }
+}
--- Conta Bancaria/Program.cs
+++ Conta Bancaria/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Conta_Bancaria;
+using System.Drawing;
+
+var contaBanc = new ContaBancaria();
+var resposta = "";
+
+Console.Write("Qual o nome do titular da conta? ");
+contaBanc.SetTitular(Console.ReadLine() ?? "");
+
+Console.WriteLine("\nPara utilizar as funções da conta, precisa fazer um desposito acima de R$1,00");
+
+Console.Write("Digite o valor que vai ser depositado: ");
+contaBanc.Depositar(decimal.Parse(Console.ReadLine()));
+
+do
+{
+    Console.Write("\nDeseja fazer um saque ou um deposito? (1 - saque | 2 - deposito) ");
+    var opcao = char.Parse(Console.ReadLine());
+
+    while (opcao != '1' && opcao != '2')
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("\nErro. Digite corretamente!\n");
+        Console.ResetColor();
+
+        Console.Write("Deseja fazer um saque ou um deposito? (1 - saque | 2 - deposito) ");
+        opcao = char.Parse(Console.ReadLine());
+    }
+
+    if (opcao == '1')
+    {
+        Console.Write("Digite o valor que vai ser Sacado: ");
+        contaBanc.Sacar(decimal.Parse(Console.ReadLine()));
+    }
+    else
+    {
+        Console.Write("Digite o valor que vai ser depositado: ");
+        contaBanc.Depositar(decimal.Parse(Console.ReadLine()));
+    }
+
+    Console.Clear();
+    contaBanc.Extrato();
+    Console.Write("\nDeseja depositar ou fazer um saque novamente? (s/n) ");
+    resposta = Console.ReadLine();
+} while (resposta == "s");
+
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Obrigado por utilizar nossos serviços. Volte sempre!");
+Console.ResetColor();
+
+
+
